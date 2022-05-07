c6c73fb6a7c552f220aae1a06dcc84af24c39e67
William Steele <johnsteele2009@live.co.uk>
2022-05-07 15:53:55 +0100

    Added main.py

---
+++ mablibs/main.py
@@ -0,0 +1,14 @@
+import randomization
+from templates import Template
+import string
+import itertools as it
+from time import time
+import random
+
+def main():
+    r = randomization.TrimerRandomization({0: 'A C D E'.split(), 3: 'A C D E'.split(), 6: 'A C D E'.split()})
+    t = Template('CCCTTTTATAAAGGTGGGAGA')
+    for m in r.get_mutations():
+        print(t.translate(t.get_mutant(m)))
+ 
+if __name__ == '__main__':
