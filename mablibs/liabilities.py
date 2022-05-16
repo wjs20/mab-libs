2b2de2db34dd9b5271b9894b8e4ee382976ca1cb
William Steele <johnsteele2009@live.co.uk>
2022-05-16 16:15:20 +0100

    Removed DNA search patterns for amino acid liabilities

--- mablibs/liabilities.py
+++ mablibs/liabilities.py
@@ -1,49 +1,33 @@
 import re
-from collections import namedtuple 
+from collections import namedtuple
 
-# amino acid - codon regexprs 
-N = 'AA[TC]'
-G = 'GG[TCAG]'
-S = 'TC[TCAG]|AG[TC]'
-A = 'GC[TCAG]'
-D = 'GA[TC]'
-P = 'CC[TCAG]'
-M = 'ATG'
-T = 'AC[TCAG]'
+amino_acid_liability_specification = (
+    ("GLYCOSYLATION_MOTIF", r"N[^P][ST]"),
+    ("DEAMIDATION_MOTIF", r"N[GSA]"),
+    ("ISOMERIZATION_MOTIF", r"D[GS]"),
+    ("CLEAVAGE_MOTIF", r"DP"),
+    ("OXIDATION_MOTIF", r"M"),
+)
 
-# antibody liabilities identified by their codon regexrps
-#liability_specification = (
-#        ('GLYCOSYLATION_MOTIF', r'AA[TC](?!CC[TCAG])[ATCG]{3}(TC[TCAG]|AG[TC]|AC[TCAG])'),
-#        ('DEAMIDATION_MOTIF', r'AA[TC](GG[TCAG]|TC[TCAG]|AG[TC]|AC[TCAG])'),
-#        ('ISOMERIZATION_MOTIF', r'GA[TC](GG[TCAG]|TC[TCAG]|AG[TC])'),
-#        ('CLEAVAGE_MOTIF', r'GA[TC]CC[TCAG]'),
-#        ('OXIDATION_MOTIF', r'ATG')
-#    )
+amino_acid_liability_regex = re.compile(
+    "|".join("(?P<%s>%s)" % pair for pair in amino_acid_liability_specification)
+)
 
-liability_specification = (
-        ('GLYCOSYLATION_MOTIF', r'N[^P][ST]'),
-        ('DEAMIDATION_MOTIF',   r'N[GSA]'),
-        ('ISOMERIZATION_MOTIF', r'D[GS]'),
-        ('CLEAVAGE_MOTIF',      r'DP'),
-        ('OXIDATION_MOTIF',     r'M')
-    )
+Liability = namedtuple("Liability", "kind match start end")
 
-liability_regex = re.compile('|'.join('(?P<%s>%s)' % pair for pair in liability_specification))
-
-Liability = namedtuple('Liability', 'kind match start end')
 
 def find_liabilities(seq, report=False):
-    matches = list(liability_regex.finditer(seq))
-    if not matches: return 
-    if not report: return True
+    matches = list(amino_acid_liability_regex.finditer(seq))
+    if not matches:
+        return
+    if not report:
+        return True
     return [
-        Liability(mo.lastgroup, mo.group(0), mo.start(), mo.end())
-        for mo in matches
+        Liability(mo.lastgroup, mo.group(0), mo.start(), mo.end()) for mo in matches
     ]
 
-if __name__ == '__main__':
-    test_seqs = [
-            'ATG'
-        ]
+
+if __name__ == "__main__":
+    test_seqs = ["ATG"]
 
     print(find_liabilities(test_seqs[0]))
