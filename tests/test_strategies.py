6e3329c2fab62a9d8941f34c4ebffacab29ca499
William Steele <johnsteele2009@live.co.uk>
2022-05-18 13:20:31 +0100

    Refactored RandomizationStrategy from class heirachy to single class

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,82 +1,79 @@
+from itertools import islice
 from mablibs.strategies import *
 import pytest
 
 
 @pytest.fixture()
 def position_residue_mapping():
-    return {1: ["A", "C", "D", "E"], 3: ["A", "C", "D", "E"], 6: ["A", "C", "D", "E"]}
+    return dict(zip(range(1, 5), ["A B C D".split()] * 4))
 
 
-@pytest.fixture
-def twomerrandomization(position_residue_mapping):
-    return NmerRandomizationStrategy(2, position_residue_mapping)
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (
+            2,
+            [
+                ((1, "A"), (2, "A")),
+                ((1, "A"), (2, "B")),
+                ((1, "A"), (2, "C")),
+                ((1, "A"), (2, "D")),
+            ],
+        ),
+        (
+            3,
+            [
+                ((1, "A"), (2, "A"), (3, "A")),
+                ((1, "A"), (2, "A"), (3, "B")),
+                ((1, "A"), (2, "A"), (3, "C")),
+                ((1, "A"), (2, "A"), (3, "D")),
+            ],
+        ),
+        (
+            4,
+            [
+                ((1, "A"), (2, "A"), (3, "A"), (4, "A")),
+                ((1, "A"), (2, "A"), (3, "A"), (4, "B")),
+                ((1, "A"), (2, "A"), (3, "A"), (4, "C")),
+                ((1, "A"), (2, "A"), (3, "A"), (4, "D")),
+            ],
+        ),
+    ],
+)
+def test_randomization_strategy_get_mutations(
+    position_residue_mapping, test_input, expected
+):
+    randomization = RandomizationStrategy(position_residue_mapping, test_input)
+    assert list(it.islice(randomization.get_mutations(), 4)) == expected
 
 
-@pytest.fixture
-def randomtrimerrandomization(position_residue_mapping):
-    return RandomTrimerRandomizationStrategy(position_residue_mapping)
+@pytest.mark.parametrize("test_input,expected", [(2, 96), (3, 256), (4, 256)])
+def test_randomization_strategy_library_size(
+    position_residue_mapping, test_input, expected
+):
+    randomization = RandomizationStrategy(position_residue_mapping, test_input)
+    assert randomization.library_size == expected
 
 
-def test_randomization_zip_keys_to_vals(position_residue_mapping):
+def test_randomization_strategy_zip_keys_to_vals(position_residue_mapping):
     r = RandomizationStrategy(position_residue_mapping)
-    assert r.zip_keys_to_vals(position_residue_mapping) == [
-        [(1, "A"), (1, "C"), (1, "D"), (1, "E")],
-        [(3, "A"), (3, "C"), (3, "D"), (3, "E")],
-        [(6, "A"), (6, "C"), (6, "D"), (6, "E")],
+    assert r._zip_keys_to_vals(position_residue_mapping) == [
+        [(1, "A"), (1, "B"), (1, "C"), (1, "D")],
+        [(2, "A"), (2, "B"), (2, "C"), (2, "D")],
+        [(3, "A"), (3, "B"), (3, "C"), (3, "D")],
+        [(4, "A"), (4, "B"), (4, "C"), (4, "D")],
     ]
 
 
-def test_twomerrandomization_get_mutations(twomerrandomization):
-    assert twomerrandomization.get_mutations(generator=False, first=10) == [
-        ((1, "A"), (3, "A")),
-        ((1, "A"), (3, "C")),
-        ((1, "A"), (3, "D")),
-        ((1, "A"), (3, "E")),
-        ((1, "A"), (6, "A")),
-        ((1, "A"), (6, "C")),
-        ((1, "A"), (6, "D")),
-        ((1, "A"), (6, "E")),
-        ((1, "C"), (3, "A")),
+def test_randomization_strategy_sample(position_residue_mapping):
+    r = RandomizationStrategy(position_residue_mapping, n=2)
+    assert list(r.sample(3, seed=42)) == [
+        ((1, "A"), (2, "C")),
         ((1, "C"), (3, "C")),
+        ((2, "D"), (3, "B")),
     ]
 
 
-def test_randomtrimerrandomization_get_mutations(randomtrimerrandomization):
-    assert randomtrimerrandomization.get_mutations(generator=False, first=10) == [
-        ((1, "A"), (3, "A"), (6, "A")),
-        ((1, "A"), (3, "A"), (6, "C")),
-        ((1, "A"), (3, "A"), (6, "D")),
-        ((1, "A"), (3, "A"), (6, "E")),
-        ((1, "A"), (3, "C"), (6, "A")),
-        ((1, "A"), (3, "C"), (6, "C")),
-        ((1, "A"), (3, "C"), (6, "D")),
-        ((1, "A"), (3, "C"), (6, "E")),
-        ((1, "A"), (3, "D"), (6, "A")),
-        ((1, "A"), (3, "D"), (6, "C")),
-    ]
-
-
-def test_randomtrimerrandomization_library_size(randomtrimerrandomization):
-    assert randomtrimerrandomization.library_size == 64
-
-
-def test_twomerrandomization_library_size(twomerrandomization):
-    assert twomerrandomization.library_size == 48
-
-
-def test_sample(twomerrandomization):
-    assert [mutations for mutations in twomerrandomization.sample(10, seed=42)] == [
-        ((1, "A"), (3, "C")),
-        ((1, "A"), (6, "A")),
-        ((1, "C"), (3, "D")),
-        ((1, "C"), (6, "C")),
-        ((1, "D"), (6, "A")),
-        ((1, "E"), (6, "D")),
-        ((3, "A"), (6, "A")),
-        ((3, "A"), (6, "E")),
-        ((3, "D"), (6, "D")),
-    ]
-
-
-def test_mul_lens(twomerrandomization):
-    assert twomerrandomization._mul_lens([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 27
+def test_randomization_strategy_mul_lens(position_residue_mapping):
+    r = RandomizationStrategy(position_residue_mapping, n=2)
+    assert r._mul_lens([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 27
