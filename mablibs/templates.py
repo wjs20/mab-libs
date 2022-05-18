e91b94e5918af43f11fce57420365804fdd667ef
William Steele <johnsteele2009@live.co.uk>
2022-05-18 18:31:23 +0100

    Refactored to remove mutation logic out of Template class

--- mablibs/templates.py
+++ mablibs/templates.py
@@ -2,6 +2,7 @@ import json
 import operator
 import pkgutil
 from typing import List, Tuple
+from dataclasses import dataclass
 
 AA2CODON = json.loads(
     pkgutil.get_data("mablibs", "amino_acid2codon.json").decode("utf-8")
@@ -11,43 +12,15 @@ CODON2AA = json.loads(
     pkgutil.get_data("mablibs", "codon2amino_acid.json").decode("utf-8")
 )
 
-
-def translate(sequence_nt: str) -> str:
-    """Method converts nucleotide sequences to amino acid sequences"""
-    seq_len = len(sequence_nt)
-    assert seq_len % 3 == 0, "Ensure sequence contains complete reading frame"
-    return "".join(
-        [
-            CODON2AA[codon]
-            for codon in [sequence_nt[i : i + 3] for i in range(0, seq_len, 3)]
-        ]
-    )
-
-
+@dataclass
 class Template:
-    def __init__(self, sequence_nt: str) -> None:
-        self.sequence_nt = sequence_nt
-        self.sequence_aa = translate(sequence_nt)
+    nucleotides: str
 
-    def get_mutant(self, mutations: List[Tuple[str]]) -> str:
-        """Method returns the template sequence with the residues at the
-        positions specified at initialization replaced by residues listed in
-        'mutations.'
-        """
-        codons_copy = list(self.codons())
-        for position, residue in mutations:
-            replacement_codon = AA2CODON[residue][0]
-            codons_copy[position] = replacement_codon
-        return "".join(codons_copy)
+    def codons(self) -> List[str]:
+        return [self.nucleotides[i : i + 3] for i in range(0, len(self.nucleotides), 3)]
 
-    def codons(self):
-        return [self.sequence_nt[i : i + 3] for i in range(0, len(self.sequence_nt), 3)]
-
-    def __repr__(self):
-        return f"Amino acids: {self.sequence_aa}\nNucleotides: {self.sequence_nt}"
-
-
-if __name__ == "__main__":
-    t = Template("TTTCTTATTCCTCCCATTAATGGTCATGAACTTCTC")
-    assert t.sequence_aa == "FLIPPINGHELL"
-    assert translate(t.get_mutant([(1, "T")])) == "FTIPPINGHELL"
+    def translation(self) -> str:
+        try:
+            return "".join([CODON2AA[codon] for codon in self.codons()])
+        except KeyError as e:
+            raise NotImplementedError(f'Amino acid could not be found, error: {e}')
