d8972e592bb13d6aab910bbaea3fe7543fc8d155
William Steele <johnsteele2009@live.co.uk>
2022-05-19 14:32:43 +0100

    Changed translation method to amino_acids property

--- mablibs/templates.py
+++ mablibs/templates.py
@@ -12,7 +12,8 @@ class Template:
     def codons(self) -> List[str]:
         return [self.nucleotides[i : i + 3] for i in range(0, len(self.nucleotides), 3)]
 
-    def translation(self) -> str:
+    @property
+    def amino_acids(self) -> str:
         try:
             return "".join([codons.CODON2AA[codon] for codon in self.codons()])
         except KeyError as e:
