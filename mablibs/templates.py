056763e46491d355f50f5753abc65f3a7084f7d8
William Steele <johnsteele2009@live.co.uk>
2022-06-01 16:39:43 +0100

    Removed unecessary imports and switched Template from dataclass to regular class

--- mablibs/templates.py
+++ mablibs/templates.py
@@ -1,20 +1,26 @@
-import json
-import operator
-import pkgutil
-from typing import List, Tuple
-from dataclasses import dataclass
+from typing import List
+
 from mablibs import codons
 
-@dataclass
-class Template:
-    nucleotides: str
 
-    def codons(self) -> List[str]:
-        return [self.nucleotides[i : i + 3] for i in range(0, len(self.nucleotides), 3)]
+class Template:
+    def __init__(self, nucleotides) -> None:
+        self.nucleotides = nucleotides
 
     @property
     def amino_acids(self) -> str:
         try:
             return "".join([codons.CODON2AA[codon] for codon in self.codons()])
         except KeyError as e:
-            raise NotImplementedError(f'Amino acid could not be found, error: {e}')
+            raise NotImplementedError(
+                f"Amino acid could not be found, error: {e}"
+            )
+
+    def codons(self) -> List[str]:
+        return [
+            self.nucleotides[i : i + 3]
+            for i in range(0, len(self.nucleotides), 3)
+        ]
+
+    def __repr__(self):
+        return f"Template({self.nucleotides})"
