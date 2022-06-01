7a38cff7bbb0c9ab0e0ee2daa83c9e253bea3cd0
William Steele <johnsteele2009@live.co.uk>
2022-06-01 16:40:53 +0100

    Changed position residue mapping fixture to only contain conventional amino acids

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,11 +1,17 @@
 from itertools import islice
-from mablibs.strategies import *
+from typing import Dict
+
 import pytest
+from mablibs.strategies import *
 
 
 @pytest.fixture()
-def position_residue_mapping():
-    return dict(zip(range(1, 5), ["A B C D".split()] * 4))
+def position_residue_mapping() -> Dict[int, str]:
+    return dict(zip(range(1, 5), ["A C D E".split()] * 4))
+
+
+def test_randomization_strategy(position_residue_mapping) -> None:
+    randomization = RandomizationStrategy(position_residue_mapping)
 
 
 @pytest.mark.parametrize(
@@ -15,65 +21,67 @@ def position_residue_mapping():
             2,
             [
                 ((1, "A"), (2, "A")),
-                ((1, "A"), (2, "B")),
                 ((1, "A"), (2, "C")),
                 ((1, "A"), (2, "D")),
+                ((1, "A"), (2, "E")),
             ],
         ),
         (
             3,
             [
                 ((1, "A"), (2, "A"), (3, "A")),
-                ((1, "A"), (2, "A"), (3, "B")),
                 ((1, "A"), (2, "A"), (3, "C")),
                 ((1, "A"), (2, "A"), (3, "D")),
+                ((1, "A"), (2, "A"), (3, "E")),
             ],
         ),
         (
             4,
             [
                 ((1, "A"), (2, "A"), (3, "A"), (4, "A")),
-                ((1, "A"), (2, "A"), (3, "A"), (4, "B")),
                 ((1, "A"), (2, "A"), (3, "A"), (4, "C")),
                 ((1, "A"), (2, "A"), (3, "A"), (4, "D")),
+                ((1, "A"), (2, "A"), (3, "A"), (4, "E")),
             ],
         ),
     ],
 )
 def test_randomization_strategy_get_mutations(
     position_residue_mapping, test_input, expected
-):
+) -> None:
     randomization = RandomizationStrategy(position_residue_mapping, test_input)
     assert list(it.islice(randomization.get_mutations(), 4)) == expected
 
 
 @pytest.mark.parametrize("test_input,expected", [(2, 96), (3, 256), (4, 256)])
-def test_randomization_strategy_library_size(
+def test_randomization_strategy_len(
     position_residue_mapping, test_input, expected
-):
+) -> None:
     randomization = RandomizationStrategy(position_residue_mapping, test_input)
-    assert randomization.library_size == expected
+    assert len(randomization) == expected
 
 
-def test_randomization_strategy_zip_keys_to_vals(position_residue_mapping):
+def test_randomization_strategy_zip_keys_to_vals(
+    position_residue_mapping,
+) -> None:
     r = RandomizationStrategy(position_residue_mapping)
-    assert r._zip_keys_to_vals(position_residue_mapping) == [
-        [(1, "A"), (1, "B"), (1, "C"), (1, "D")],
-        [(2, "A"), (2, "B"), (2, "C"), (2, "D")],
-        [(3, "A"), (3, "B"), (3, "C"), (3, "D")],
-        [(4, "A"), (4, "B"), (4, "C"), (4, "D")],
+    assert r.zip_keys_to_vals(position_residue_mapping) == [
+        [(1, "A"), (1, "C"), (1, "D"), (1, "E")],
+        [(2, "A"), (2, "C"), (2, "D"), (2, "E")],
+        [(3, "A"), (3, "C"), (3, "D"), (3, "E")],
+        [(4, "A"), (4, "C"), (4, "D"), (4, "E")],
     ]
 
 
-def test_randomization_strategy_sample(position_residue_mapping):
+def test_randomization_strategy_sample(position_residue_mapping) -> None:
     r = RandomizationStrategy(position_residue_mapping, n=2)
     assert list(r.sample(3, seed=42)) == [
-        ((1, "A"), (2, "C")),
-        ((1, "C"), (3, "C")),
-        ((2, "D"), (3, "B")),
+        ((1, "A"), (2, "D")),
+        ((1, "D"), (3, "D")),
+        ((2, "E"), (3, "C")),
     ]
 
 
-def test_randomization_strategy_mul_lens(position_residue_mapping):
+def test_randomization_strategymul_lens(position_residue_mapping) -> None:
     r = RandomizationStrategy(position_residue_mapping, n=2)
-    assert r._mul_lens([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 27
+    assert r.mul_lens([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == 27
